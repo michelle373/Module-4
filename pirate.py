9bcf6c30c0d0c9a52a7bb327b08dbe5e32b66c62
michelle373 <mpatlan@icstars.org>
2022-07-31 20:36:20 -0500

    Add files via upload
    adding all the files

---
+++ pirate.py
@@ -0,0 +1,5 @@
+greeting = input("Hello, possible pirate! What's the password?")#mising "
+if greeting in ["Arrr!"]: #incorrect )
+	print("Go away, pirate.")
+else: #inncorrect else
+	print("Greetings, hater of pirates!")#incorrect indent
---
+++ time.py
@@ -0,0 +1,8 @@
+currentTimeStr = input("What is the current time (in hours 0-23)?")
+waitTimeStr = input("How many hours do you want to wait")#missing )
+
+currentTimeInt = int(currentTimeStr)#incorrect object
+waitTimeInt = int(waitTimeStr)#incorrect object
+
+finalTimeInt = currentTimeInt + waitTimeInt
+print(finalTimeInt)#incorrect object
